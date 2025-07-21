47f31ec33866597c7362f30a8ffbe3a0911b2a70
Joseph Murrieta <amurrie2@charlotte.edu>
2025-07-21 13:33:29 -0400

    chore: add comments to explain each endpoint

--- main.py
+++ main.py
@@ -9,14 +9,17 @@ class Item(BaseModel):
     price: float
     is_offer: Union[bool, None] = None
 
+# Root endpoint that returns a greeting
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+#GET endpoint that returns item_id and optional query param
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+# PUT endpoint that updates an item with JSON data
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
